7fd6ec494b7ea8c40cf599e6967084099786e643
jannatul-jahan <jannatul.jahan@bjitacademy.com>
2023-09-14 09:31:28 +0600

    added

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const UserRouter = require("./routes/User");
 const ProductRouter = require("./routes/Product");
-const TransactionRouter = require("./routes/Transaction");
-const CartRouter = require("./routes/Cart");
 // const AuthRouter = require("./routes/Auth");
-// const ProductModel = require("../model/Product");
 const dotenv = require("dotenv");
 const databaseConnection = require("./config/database");
 
@@ -19,8 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/products", ProductRouter);
 // app.use("/users", UserRouter);
-app.use("/transactions", TransactionRouter);
-app.use("/cart", CartRouter);
 // app.use("/users", AuthRouter);
 
 
