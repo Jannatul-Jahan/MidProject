5e7e36db2ef389fac3cbdd057358ef915cebae43
jannatul-jahan <jannatul.jahan@bjitacademy.com>
2023-09-19 10:01:20 +0600

    file changed

--- routes/Review.js
+++ routes/Review.js
@@ -3,11 +3,11 @@ const routes = express();
 const ReviewController = require("../controller/ReviewController");
 const { isAuthenticated, isUser, isSpecificUser } = require("../middleware/auth");
 
-//routes.get("/all", DiscountController.getAll);
+
 routes.post("/add", isAuthenticated, isUser, isSpecificUser, ReviewController.addReview);
 routes.patch("/update", isAuthenticated, isUser, isSpecificUser, ReviewController.updateReview);
 routes.delete("/delete", isAuthenticated, isUser, isSpecificUser, ReviewController.deleteReview);
-//routes.patch("/update", isAuthenticated, isUser, ReviewController.update);
+
 
 
 module.exports = routes;
